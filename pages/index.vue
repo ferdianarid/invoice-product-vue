24f0a054e195b77a01b0fa94d0630a418b017b50
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2021-11-30 22:58:13 +0700

    update assets file

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="main px-4 pt-4 md:px-24 md:pt-8">
+	<div class="main max-w-[1440px] m-auto px-4 pt-4 md:px-24 md:pt-8">
 		<Navbar />
 		<h1>Homepage</h1>
 	</div>
@@ -8,19 +8,38 @@
 <script>
 // import Register Component
 import Register from "./auth/Register.vue"
+
+// import Navbar Components
 import Navbar from "~/components/Navbar.vue"
+
 export default {
 	name: "Homepage",
 	components: {
-    Register,
-    Navbar
-}
+		Register,
+		Navbar
+	},
+	head: {
+		title: "Timvoice - Homepage",
+		meta: [
+			{ charset: "utf-8" },
+			{ name: "viewport", content: "width=device-width, initial-scale=1"},
+			{
+				hid: "invoice product",
+				name: "invoice product - homepage",
+				content: "invoice product - homepage"
+			}
+		],
+		link: [
+			{ rel: "icon", type: "image/x-icon", href: "/favicon.ico"},
+			{ rel: "stylesheets", href: "https://fonts.googleapis.com/css2?family=Mulish:wght@400;500;600;700;800&display=swap"}
+		]
+	}
 }
 </script>
 
 <style>
 	* {
-		font-family: "SF Pro Display", sans-serif;
+		font-family: "Mulish", sans-serif;
 		box-sizing: border-box;
 		margin: 0;
 		padding: 0;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    mode: "jit",
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
 }
