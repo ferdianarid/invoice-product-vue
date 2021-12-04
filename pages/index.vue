fd4e7012559b22ffa3c591ac0b6883001855e1dd
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2021-12-04 12:07:40 +0700

    update homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
 	<div class="background-gradient">
 		<div class="w-full main max-w-[1440px] m-auto px-4 pt-4 md:px-24 md:pt-8">
+			<!-- Navbar Components -->
 			<Navbar />
 			<!-- Header Section -->
 			<div class="w-full flex flex-col md:flex-row px-0 md:px-[105px] pb-8">
@@ -24,7 +25,8 @@
 				</div>
 			</div>
 			<!-- Partner Section -->
-			<div class="w-full flex flex-col md:flex-row items-center my-[60px]">
+			<Slider />
+			<!-- <div class="w-full flex flex-col md:flex-row items-center my-[60px]">
 				<div class="w-1/4 flex justify-start mt-10 md:mt-0">
 					<img src="../static/Partner/Client-Logo2.svg" alt="partner1" />
 				</div>
@@ -37,7 +39,7 @@
 				<div class="w-1/4 mt-10 md:mt-0">
 					<img src="../static/Partner/Client-Logo1.svg" alt="partner1" />
 				</div>
-			</div>
+			</div> -->
 			<!-- Feature Section -->
 			<div class="w-full flex flex-col md:flex-row my-16 md:my-24">
 				<div class="w-full md:w-1/2 mr-[100px] flex items-center">
@@ -117,47 +119,8 @@
 			<!-- Testimonials Section -->
 			<div class="w-full my-16 md:my-24">
 				<h1 class="text-[2.75rem] leading-[1.2] md:text-heading-xl mt-14 heading mb-12 text-center">What Did They Say</h1>
-				<div class="flex flex-col md:flex-row">
-					<!-- Testimonial 1 -->
-					<div class="w-full md:w-[388px] mt-0 md:mt-0 bg-[#F7F8FD] rounded-[24px] p-8">
-						<p class="text-lg-regular text-gray-500">“Completely wonderful. If you freelance you have to check it out. Really lovely experience.”</p>
-						<div class="w-full flex justify-between">
-							<div class="w-1/4 pt-4">
-								<img src="../static/Avatar-Testimonial1.png" alt="avatar1">
-							</div>
-							<div class="w-3/4 pl-2 pt-4">
-								<p class="text-xl-regular fontBold pb-1">Elisa Nurjaman</p>
-								<p class="text-lg-regular fontBold text-gray-500">CEO  Ungu Studio</p>
-							</div>
-						</div>
-					</div>
-					<!-- Testimonial 2 -->
-					<div class="w-full md:w-[388px] mt-4 md:mt-0 bg-[#F7F8FD] rounded-[24px] p-8 mx-0 md:mx-8">
-						<p class="text-lg-regular text-gray-500">“For all my Freelancer/Small Agency followers, I wish I’d discovered this long ago.”</p>
-						<div class="w-full flex justify-between">
-							<div class="w-1/4 pt-4">
-								<img src="../static/Avatar-Testimonial2.png" alt="avatar2">
-							</div>
-							<div class="w-3/4 pl-2 pt-4">
-								<p class="text-xl-regular fontBold pb-1">Ujang Fahreza</p>
-								<p class="text-lg-regular fontBold text-gray-500">UI/UX Freelancer</p>
-							</div>
-						</div>
-					</div>
-					<!-- Testimonial 3 -->
-					<div class="w-full md:w-[388px] mt-4 md:mt-0 bg-[#F7F8FD] rounded-[24px] p-8">
-						<p class="text-lg-regular text-gray-500">"Anyone who have small businesses should use this tool for contracts/payment."</p>
-						<div class="w-full flex justify-between">
-							<div class="w-1/4 pt-4">
-								<img src="../static/Avatar-Testimonial3.png" alt="avatar3">
-							</div>
-							<div class="w-3/4 pl-2 pt-4">
-								<p class="text-xl-regular fontBold pb-1">Bambang W</p>
-								<p class="text-lg-regular fontBold text-gray-500">Frontend Developer</p>
-							</div>
-						</div>
-					</div>
-				</div>
+				<Swipers />
+				
 			</div>
 			<!-- Call to Action -->
 			<div class="w-full mt-12 mb-24 rounded-lg py-16 md:py-20 px-4 md:px-[105px] flex flex-col md:flex-row justify-between">
@@ -177,60 +140,8 @@
 				</div>
 			</div>
 		</div>
-		<div class="w-full max-w-[1440px] m-auto">
-		<!-- Footer Section -->
-			<div class="w-full h-auto md:h-[403px] bg-[#202E63] pt-16 md:pt-24 pb-12 flex flex-col md:flex-row justify-between px-4 md:px-24">
-				<div class="w-full md:w-[40%]">
-					<p class="text-lg-regular text-[#F7F8FD]">Timvoice is an Invoice generator for small business, freelancers and startups.</p>
-					<!-- Social Media -->
-					<div class="mt-6 flex">
-						<img class="mr-2 opacity-60 hover:opacity-100 transition-all" src="../static/SocialMedia/Facebook.svg" alt="facebook" height="36px">
-						<img class="mr-2 opacity-60 hover:opacity-100 transition-all" src="../static/SocialMedia/Instagram.svg" alt="instagram" height="36px">
-						<img class="mr-2 opacity-60 hover:opacity-100 transition-all" src="../static/SocialMedia/Linkedin.svg" alt="linkedin" height="36px">
-					</div>
-				</div>
-				<div class="w-full md:w-[60%] flex justify-between mt-12 md:mt-0">
-					<!-- Tab Menu -->
-					<div class="products">
-						<p class="text-lg-regular text-white fontBold pb-4">Products</p>
-						<p class="text-lg-regular text-white py-1 md:py-2">Home</p>
-						<p class="text-lg-regular text-white py-1 md:py-2">Features</p>
-						<p class="text-lg-regular text-white py-1 md:py-2">Pricing</p>
-						<p class="text-lg-regular text-white py-1 md:py-2">What's New</p>
-					</div>
-					<!-- Tab Menu -->
-					<div class="company">
-						<p class="text-lg-regular text-white fontBold pb-4">Company</p>
-						<p class="text-lg-regular text-white py-1 md:py-2">About</p>
-						<p class="text-lg-regular text-white py-1 md:py-2">Blog</p>
-						<p class="text-lg-regular text-white py-1 md:py-2">Contact Us</p>
-						<p class="text-lg-regular text-white py-1 md:py-2">Careers</p>
-					</div>
-					<!-- Tab Menu -->
-					<div class="support">
-						<p class="text-lg-regular text-white fontBold pb-4">Support</p>
-						<p class="text-lg-regular text-white py-1 md:py-2">Customer Service</p>
-						<p class="text-lg-regular text-white py-1 md:py-2">Help Center</p>
-						<p class="text-lg-regular text-white py-1 md:py-2">FAQ</p>
-						<p class="text-lg-regular text-white py-1 md:py-2">Tutorial</p>
-					</div>
-				</div>
-			</div>
-		<div class="w-full bg-[#202E63] px-4 md:px-24 pb-12">
-			<div class="w-full border-t border-white pt-12 flex justify-between">
-				<div class="flex w-full justify-center md:justify-between flex-col-reverse items-center md:flex-row">
-					<div class="w-full md:w-[30%] mt-8 md:mt-0 flex justify-center md:justify-start">
-						<img src="../static/timvoice-white.svg" alt="invoiceLogo">
-					</div>
-					<div class="w-full md:w-[70%] termsprivacy flex flex-col md:flex-row text-center md:text-left justify-center md:justify-end">
-						<p class="text-lg-regular text-white fontBold pb-2 md:pb-0 pl-0 md:pl-8">Terms of Service</p>
-						<p class="text-lg-regular text-white fontBold pb-2 md:pb-0 pl-0 md:pl-8">Privacy Policy</p>
-						<p class="text-lg-regular text-white pl-0 md:pl-8">&copy; 2021 Timvoice. All Right Reserved.</p>
-					</div>
-				</div>
-			</div>
-			</div>
-		</div>
+		<!-- Footer Section-->
+		<Footer />
 	</div>
 </template>
 
@@ -250,6 +161,12 @@ import Benefits from "~/components/Benefits.vue"
 // import main features
 import mainFeatures from "../apis/mainFeatures.json"
 
+// import Footer Components
+import Footer from "../components/Footer.vue"
+
+// import Slider Components
+import Slider from "~/components/Slider.vue"
+
 export default {
 	name: "Homepage",
 	data() {
@@ -258,11 +175,13 @@ export default {
 		}
 	},
 	components: {
-		Register,
-		Navbar,
-		PrimaryButton,
-		Benefits
-	},
+    Register,
+    Navbar,
+    PrimaryButton,
+    Benefits,
+    Footer,
+    Slider
+},
 	head: {
 		title: "Timvoice - Homepage",
 		meta: [
