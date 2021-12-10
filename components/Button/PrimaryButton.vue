c3410e9345b32486eb09f60f38b87e29a89ffc89
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2021-12-10 14:58:09 +0700

    update components

--- components/Button/PrimaryButton.vue
+++ components/Button/PrimaryButton.vue
@@ -1,8 +1,7 @@
 <template lang="">
-              <NuxtLink :to="linkUrl" class="text-center font-bold ml-0 md:ml-1 no-underline inline-block px-4 py-3 leading-none bg-blue-800 border-blue-800 border rounded text-white hover:border-transparent hover:bg-white hover:text-blue-800 mt-4 sm:mt-0">
-                     <p class="text-lg font-bold">{{ buttonTitle }}</p>
-              </NuxtLink>
-       </div>
+       <NuxtLink :to="linkUrl" class="text-center font-bold ml-0 md:ml-1 no-underline inline-block px-4 py-3 leading-none bg-blue-800 border-blue-800 border rounded text-white hover:border-transparent hover:bg-white hover:text-blue-800 mt-4 sm:mt-0">
+              <p class="text-lg font-bold">{{ buttonTitle }}</p>
+       </NuxtLink>
 </template>
 <script>
 export default {
---
+++ components/Button/SecondaryButton.vue
@@ -0,0 +1,14 @@
+<template lang="">
+       <NuxtLink :to="linkUrls" class="h-auto w-auto bg-[#FFFFFF] rounded-md">
+              <p class="text-[#405DC7] font-bold py-2 px-4 text-lg-bold">{{ buttonTitle }}</p>
+       </NuxtLink>
+</template>
+<script>
+export default {
+       name: "SecondaryButton",
+       props: ['buttonsTitle','linkUrl']
+}
+</script>
+<style lang="">
+       
+</style>
